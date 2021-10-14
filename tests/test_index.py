896952b0bd14139cd15294a258f95c0cd2c1977d
Avinash Babu <65462028+avinash2632@users.noreply.github.com>
2021-10-14 19:45:41 +0530

    Update test_index.py

--- tests/test_index.py
+++ tests/test_index.py
@@ -2,5 +2,5 @@ import requests
 
 
 def test_index():
-    response = requests.get("http://18.236.144.81:80/")
+    response = requests.get("http://18.236.144.81:80")
     assert response.status_code == 200
